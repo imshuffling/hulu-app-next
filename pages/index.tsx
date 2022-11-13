c764d05dc4e4a796dd8cda9e8e0ce6c27a1cc793
David Riches <david.riches@appnovation.com>
2022-11-13 20:31:05 +0000

    fucking typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
 
-export default function Home({ results }) {
+export default function Home({ results }: any) {
   return (
     <div>
       <Head>
